03fa9d12a2485221d9557927eaf7fd3e229f3126
VonBoyager <refluxedenergy@gmail.com>
2025-12-08 07:05:37 +0800

    Add authtoken to production settings

--- sentiment_analyzer/sentiment_analyzer/settings_production.py
+++ sentiment_analyzer/sentiment_analyzer/settings_production.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'frontend',
     'ml_analysis',
