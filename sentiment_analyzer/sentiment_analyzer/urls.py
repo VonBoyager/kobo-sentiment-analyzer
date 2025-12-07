1bdcd4092a699102ed96d9567f559097c36de8a6
VonBoyager <refluxedenergy@gmail.com>
2025-12-08 07:11:46 +0800

    Add AI dependencies and disable legacy dashboard

--- sentiment_analyzer/sentiment_analyzer/urls.py
+++ sentiment_analyzer/sentiment_analyzer/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('ml/', include('ml_analysis.urls')),
+    # path('ml/', include('ml_analysis.urls')),  # Legacy dashboard disabled
     path('tenants/', include('tenants.urls')),
     path('api/', include('api.urls')),
 ]
